05dfa92856f0ad26745fb8fdef34f9f445429c8d
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2021-08-15 22:46:11 +0100

    added call to action section in landing

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,10 +6,68 @@ function toggleMobileNav(status) {
   if (!mobile_nav) return;
 
   if (status.toLowerCase() == "open") {
+    mobile_nav.classList.remove("close");
     mobile_nav.classList.add("open");
     htmlBody.classList.add("shadow");
   } else {
     mobile_nav.classList.remove("open");
+    mobile_nav.classList.add("close");
     htmlBody.classList.remove("shadow");
   }
 }
+
+/*
+$(window).on("load resize", function () {
+  var width = $(document).width();
+
+  if (width > 700) {
+    $(".project-slider").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            variableWidth: true,
+          },
+        },
+      ],
+    });
+  } else {
+    $(".project-slider").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: false,
+      settings: "unslick",
+    });
+  }
+});
+*/
+
+$(".project-slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: false,
+      },
+    },
+  ],
+});
